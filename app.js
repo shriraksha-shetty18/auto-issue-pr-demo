191e88882ff9b4b226d9b10c02a29db34ef8c314
Aish-cyber <aishwarya7a7@gmail.com>
2026-03-13 14:25:44 +0530

    Added new demo files and fixed minor issues

--- app.js
+++
@@ -1,7 +0,0 @@
-// app.js
-function addNumbers(a, b) {
-    // BUG: accidentally using subtraction instead of addition
-    return a - b;
-}
-
-console.log("Add 2 + 3 =", addNumbers(2, 3)); // Should be 5, but will print -1
